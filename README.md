15610bc9a5ed4ddfcc97bd5c3552a3e7ccf69034
Sam Webb <sam@bvaccel.com>
2016-10-28 10:18:58 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+Yotpo Email Templates
+=======
+
+An express server that uses liquid to render the Yotpo email templates in the browser, which makes for easier styling. I would advise agains changing too much in the snippets and templates unless you know which elements are and aren't read only (there are A LOT).
+
+If you would like to view a specific template, go into `layout.liquid` and change the template that is being included within `#content`.
+
+To start the server run `npm start` in your console.
---
+++ app.js
@@ -0,0 +1,43 @@
+// System Requires
+const fs = require('fs')
+
+// Package Requires
+const express = require('express')
+const expressLiquid = require ('express-liquid')
+
+// Local Requires
+const data = require('./data/user-data.json')
+
+// Server variables
+const app = express()
+const PORT = process.env.PORT || 3000
+
+// Liquid engine options
+const options = {
+  includeFile: (filename, callback) => {
+    fs.readFile(filename, 'utf8', callback)
+  },
+
+  context: expressLiquid.newContext(),
+  customTags: {},
+  traceError: false
+}
+
+// Where to serve static files from
+app.use('/static', express.static(__dirname + '/assets'))
+
+// Sets up Liquid as the view engine
+app.set('view engine', 'liquid')
+app.engine('liquid', expressLiquid(options))
+app.use(expressLiquid.middleware)
+
+// Renders the page on visiting home
+app.get('/', (req, res) => {
+  res.locals.data = data
+  res.render('layout')
+})
+
+// Runs the app
+app.listen(PORT, () => {
+  console.log('Example app listening on port 3000!')
+})
